97b619a9573cb9f93138ea8efdb8813ab54fef16
maximiliangall <maximilian.gall@fau.de>
2020-04-07 15:45:04 +0200

    integrated rpiozero libary

--- pirhandler.py
+++ pirhandler.py
@@ -1,7 +1,7 @@
 # set up all imports
-import RPi.GPIO as GPIO
-import time
+from gpiozero import MotionSensor
 from subprocess import call
+import time
 
 # parse config
 import json
@@ -13,32 +13,31 @@ REFRESH_CYCLE = data["refresh_cycle"]
 GPIO_PIN = data["gpio_pin"]
 DEBUG = data["debug"]
 
-# disable warnings
-GPIO.setwarnings(False)
-
-# setup GPIO pin setups
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(GPIO_PIN, GPIO.IN)
-
-# define a previous signal
-s_pir_prev = GPIO.input(GPIO_PIN)
+# instanciate motion sensor
+mMotionSensor = MotionSensor(GPIO_PIN)
 time.sleep(1)
 
-# Contiuous loop
+# define ScreenOn routine for callback
+def screen_on():
+        s_pir = mMotionSensor.value
+        if DEBUG:
+                print("Screen on", s_pir)
+        call(["/usr/bin/vcgencmd", "display_power", "1"])
+        time.sleep(ON_CYCLE)
+
+# define ScreenOff routine for callback
+def screen_off():
+        s_pir = mMotionSensor.value
+        if DEBUG:
+                print("Screen off", s_pir)
+        call(["/usr/bin/vcgencmd", "display_power", "0"])
+        time.sleep(REFRESH_CYCLE)
+
+# Setup callbacks
+mMotionSensor.when_motion = screen_on
+mMotionSensor.when_no_motion = screen_off
+
+#Contiuous loop to run
 while True:
-        # check input
-        s_pir = GPIO.input(GPIO_PIN)
-        
-        # Set HDMI output
-        if s_pir == 0:
-                if DEBUG:
-                        print("Screen off", s_pir)
-               	if s_pir_prev == 1:
-			call(["/usr/bin/vcgencmd", "display_power", "0"])
-                time.sleep(REFRESH_CYCLE)
-        elif s_pir == 1:
-                if DEBUG:
-                        print("Screen on", s_pir)
-                if s_pir_prev == 0:
-			call(["/usr/bin/vcgencmd", "display_power", "0"])
-                time.sleep(ON_CYCLE)
+        # setup callbacks
+        time.sleep(REFRESH_CYCLE)
